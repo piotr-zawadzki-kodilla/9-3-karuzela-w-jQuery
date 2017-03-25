efb78757c5edb456903b4c54af7cda30c8b1b17b
zaramid <piotr@zawadzki.net>
2017-03-25 21:38:34 +0100

    fixed: semicolons after functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,22 @@ $(function(){
 		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft: 0});
-	};
+	}
 
 	function moveLastSlide() {
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
 		firstItem.before(lastItem);
 		carouselList.css({marginLeft: -400});
-	};
+	}
 
 	function changeNextSlide() {
 		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-	};
+	}
 
 	function changePrevSlide() {
 		carouselList.animate({'marginLeft': 0}, 500);
-	};
+	}
 
 	var interval = setInterval(changeNextSlide, 3000);
 
